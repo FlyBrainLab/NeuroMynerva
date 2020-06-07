78e5f149158e8f9a0f3e7dd681b230436265dcfd
mkturkcan <mehmetkeremturkcan@gmail.com>
2020-06-06 23:07:51 -0400

    Working on GFX comms.

--- packages/neugfx-widget/src/widget.ts
+++ packages/neugfx-widget/src/widget.ts
@@ -11,6 +11,7 @@ const NeuGFX_CLASS_JLab = "jp-FBL-NeuGFX";
 declare global {
   interface Window {
     neurogfxWidget: any;
+    neugfx_widget: any;
     jq: any;
   }
 }
@@ -44,11 +45,65 @@ export class NeuGFXWidget extends FBLWidget implements IFBLWidget {
     this._blocker.className = "jp-FFBOLabBlock";
     this.node.appendChild(this._blocker);
     $(".jp-FFBOLabBlock").hide();
+    window.neugfx_widget = this;
     window.neurogfxWidget = this._neugfxContainer;
     window.addEventListener("mousedown", function(){$(".jp-FFBOLabBlock").show(); });
     window.addEventListener("mouseup", function(){$(".jp-FFBOLabBlock").hide(); });
     Event.prototype.stopPropagation = function(){  };
+    var _this = this;
+    let event_func = function(event: any) {
+      // console.log(event.data);
+      console.log("[NeuGFX] Input:", event.data);
+      if (event.data.messageType == 'text') {
+        console.log("[NeuGFX] message:", event.data.data);
+      }
+      if (event.data.messageType == 'alert') {
+        if (event.data.alertType == 'success') {
+          console.log('[NeuGFX] Error!', event.data.data);
+          /*(izi as any).success({
+            id: "success",
+            message: event.data.data,
+            transitionIn: 'bounceInLeft',
+            position: 'topLeft',
+          });*/
+        }
+        else if (event.data.alertType == 'error') {
+          console.log('[NeuGFX] Success!', event.data.data);
+          /*(izi as any).error({
+            id: "success",
+            message: event.data.data,
+            transitionIn: 'bounceInLeft',
+            position: 'topLeft',
+          });*/
+        }
+      }
+      if (event.data.messageType == 'NLPquery') {
+        // neu3dwidget._userAction.emit({ action: 'execute', content: { code: '_FFBOLABres = _FFBOLABClient.executeNLPquery(query="' + event.data.query + '"); _FFBOLabcomm.send(data=_FFBOLABres)' } });
+      }
+      if (event.data.messageType == 'NLPloadTag') {
+        console.log('loadTag');
+        _this.sessionContext.session.kernel.requestExecute({code: "fbl.client_manager.clients[fbl.widget_manager.widgets['${_this.id}'].client_id]['client'].executeNAquery(query='${code}')"}).done;
+        // neu3dwidget._userAction.emit({ action: 'execute', content: { code: '_FFBOLABres = _FFBOLABClient.loadTag("' + event.data.tag + '"); _FFBOLabcomm.send(data=_FFBOLABres)' } });
+      }
+      if (event.data.messageType == 'NLPaddByUname') {
+        // neu3dwidget._userAction.emit({ action: 'execute', content: { code: '_FFBOLABres = _FFBOLABClient.addByUname([' + event.data.uname + ']);' } });
+      }
+      if (event.data.messageType == 'NLPremoveByUname') {
+        // neu3dwidget._userAction.emit({ action: 'execute', content: { code: '_FFBOLABres = _FFBOLABClient.addByUname([' + event.data.uname + '], verb="remove");' } });
+      }
+      if (event.data.messageType == 'loadExperimentConfig') {
+        // neu3dwidget._userAction.emit({ action: 'execute', content: { code: '_FFBOLABres = _FFBOLABClient.loadExperimentConfig("""' + event.data.config + '""");' } });
+      }
+      if (event.data.messageType == 'Execute') {
+        // neu3dwidget._userAction.emit({ action: 'execute', content: { code: event.data.content } });
+      }
+    };
+
+    window.addEventListener('message', event_func);
+    
   }
+
+  
 
   onCommMsg(msg: any) {
     this._neugfxContainer.contentWindow.postMessage({ messageType: msg.messageType, data: msg.data }, '*');
