42160923723ebeaecf7f3e2491f1e2b38d016c84
Tingkai Liu <tl2747@columbia.edu>
2020-07-14 11:05:48 -0400

    break pre/post in piecharts

--- packages/info-widget/src/conn_svg.tsx
+++ packages/info-widget/src/conn_svg.tsx
@@ -25,57 +25,82 @@ export function ConnSVG(props: { pre: any; post: any }) {
     return a.proportion - b.proportion;
   });
 
+  let chunkWidth = 30;
   if (pre_arr.length || post_arr.length) {
     return (
-      <PieChart width={250} height={250} style={{ margin: "0 auto" }}>
-        <Pie
-          data={pre_arr}
-          isAnimationActive={false}
-          cx={100}
-          cy={100}
-          innerRadius={40}
-          outerRadius={60}
-          fill="#8884d8"
-          paddingAngle={5}
-          dataKey="proportion"
-        >
-          {pre_arr.map((_, index) => (
-            <Cell
-              key={`cell-${index}`}
-              fill={COLORS_PRE[index % COLORS_PRE.length]}
-            />
-          ))}
-        </Pie>
-        <Pie
-          data={post_arr}
-          isAnimationActive={false}
-          cx={100}
-          cy={100}
-          innerRadius={70}
-          outerRadius={90}
-          fill="#8884d8"
-          paddingAngle={5}
-          dataKey="proportion"
-        >
-          {post_arr.map((_, index) => (
-            <Cell
-              key={`cell-${index}`}
-              fill={COLORS_POST[index % COLORS_POST.length]}
-            />
-          ))}
-          <Label
-            width={30}
-            position="center"
-            content={
-              <PrePost
-                cx={100}
-                cy={100}
-                pre={props.pre.number}
-                post={props.post.number}
+      <PieChart
+        width={chunkWidth * 9}
+        height={250}
+        style={{ margin: "0 auto" }}
+      >
+        {pre_arr.length ? (
+          <Pie
+            data={pre_arr}
+            isAnimationActive={false}
+            cx={chunkWidth * 2}
+            cy={100}
+            innerRadius={chunkWidth}
+            outerRadius={chunkWidth * 2}
+            fill="#8884d8"
+            paddingAngle={5}
+            dataKey="proportion"
+          >
+            {pre_arr.map((_, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS_PRE[index % COLORS_PRE.length]}
               />
-            }
-          />
-        </Pie>
+            ))}
+            <Label
+              width={30}
+              position="center"
+              content={
+                <PrePost
+                  cx={chunkWidth * 2}
+                  cy={100}
+                  pre={true}
+                  num={props.pre.number}
+                />
+              }
+            />
+          </Pie>
+        ) : (
+          <p>No Pre</p>
+        )}
+        {post_arr.length ? (
+          <Pie
+            data={post_arr}
+            isAnimationActive={false}
+            cx={chunkWidth * 6.5}
+            cy={100}
+            innerRadius={chunkWidth}
+            outerRadius={chunkWidth * 2}
+            fill="#8884d8"
+            paddingAngle={5}
+            dataKey="proportion"
+          >
+            {post_arr.map((_, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS_POST[index % COLORS_POST.length]}
+              />
+            ))}
+            <Label
+              width={30}
+              position="center"
+              content={
+                <PrePost
+                  cx={chunkWidth * 6.5}
+                  cy={100}
+                  pre={false}
+                  num={props.post.number}
+                />
+              }
+            />
+          </Pie>
+        ) : (
+          <p>No Post</p>
+        )}
         <Tooltip
           formatter={(value, name, entry) => {
             return `${(value as number).toFixed(2)} %`;
@@ -92,31 +117,19 @@ export function ConnSVG(props: { pre: any; post: any }) {
  * Label number of synaptic partners pre/post in the same rechart label
  * @param props
  */
-function PrePost(props: { cx: number; cy: number; pre: number; post: number }) {
+function PrePost(props: { cx: number; cy: number; pre: boolean; num: number }) {
   return (
     <React.Fragment>
       <text
         x={props.cx}
-        y={props.cy - 10}
+        y={props.cy}
         fill="#3d405c"
         className="recharts-text recharts-label"
         textAnchor="middle"
         dominantBaseline="central"
       >
         <tspan alignmentBaseline="baseline" text-anchor="middle" fontSize="15">
-          Pre: {props.pre}
-        </tspan>
-      </text>
-      <text
-        x={props.cx}
-        y={props.cy + 10}
-        fill="#3d405c"
-        className="recharts-text recharts-label"
-        textAnchor="middle"
-        dominantBaseline="central"
-      >
-        <tspan alignmentBaseline="central" text-anchor="middle" fontSize="15">
-          Post: {props.post}
+          {props.pre ? <>Pre: {props.num}</> : <>Post: {props.num}</>}
         </tspan>
       </text>
     </React.Fragment>
