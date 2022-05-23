05b21603e457c8391a9ca756c88b39700e5fe839
Yiyin Zhou <yiyin@ee.columbia.edu>
2022-05-23 00:20:41 -0400

    add jupyter_packaging to install_requires

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup_args = dict(
     packages=setuptools.find_packages(),
     install_requires=[
         "jupyterlab~=3.0",
+        "jupyter_packaging"
         # "flybrainlab>=1.1.3"
     ],
     zip_safe=False,
